b593f045679bc169c7c7f8900e3357209ee66a01
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-09-21 13:40:15 -0700

    finished

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,12 +1,29 @@
 function Links({ links, remove }) {
+
+	const baseUrl = 'localhost:8080/';
+
+	const copy = (e) => {
+		const url = baseUrl + e.target.name
+  		navigator.clipboard.writeText(url);
+  		e.target.textContent = " copied!"
+  		setTimeout(() => {
+  			e.target.textContent = " copy"
+  		}, 2000)
+	}
+
+
 	return (
 		<div className="list">
 			{links.map((object, index) => (
 				<div key={index} className="cell">
 					<button className="urls"><i className="fas fa-globe"></i> {object.url}</button>
 					<div className="shortened">
-						<div  className="readonly">localhost:8080/{object.hash}</div>
-						<button className="copy">copy</button>
+						<div 
+						className="readonly">localhost:8080/{object.hash}</div>
+						<button 
+						name={object.hash}
+						onClick={copy}
+						className="copy"> copy</button>
 					</div>
 					<button 
 						name={object.url}
